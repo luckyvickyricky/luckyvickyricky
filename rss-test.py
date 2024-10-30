e8eff80464a42b08f7d543d5b0892b89cbded2cf
cukminseo <kms39273@catholic.ac.kr>
2024-10-31 02:54:47 +0900

    feat/print today_date

--- rss-test.py
+++ rss-test.py
@@ -1,5 +1,7 @@
 import feedparser
 import time
+from datetime import datetime
+
 URL = "https://def-init.tistory.com/rss"  # URL = "내블로그 주소/rss"
 RSS_FEED = feedparser.parse(URL)
 MAX_POST = 5
@@ -16,3 +18,10 @@ for idx, feed in enumerate(RSS_FEED['entries']):
         markdown_text += f"[{time.strftime('%Y/%m/%d', feed_date)} - {feed['title']}]({feed['link']}) <br/>\n"
 
 print(markdown_text)
+
+
+# 현재 날짜를 '월 이름 일, 년' 형식으로 포맷
+today_date = datetime.now().strftime("%B %d, %Y")
+
+# 메시지 출력
+print(f"# Hello, {today_date}! Let's give it our best today!")
